d3465740780ae114b78e7d7ddc08685e71fa288c
3epochs <1738547868@qq.com>
2019-04-09 11:02:11 +0800

    add ResNet

--- ResNet/resnet.py
+++ ResNet/resnet.py
@@ -10,7 +10,7 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
 
         self.residual_function = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels,kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
@@ -79,3 +79,50 @@ class ResNet(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
+
+        self.conv2 = self._make_layers(block, 64, num_block[0], 1)
+        self.conv3 = self._make_layers(block, 128, num_block[1], 2)
+        self.conv4 = self._make_layers(block, 256, num_block[2], 2)
+        self.conv5 = self._make_layers(block, 512, num_block[3], 2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layers(self, block, out_channels, num_blocks, stride):
+        layers = []
+        strides = [stride] + [1] * (num_blocks - 1)
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+        return nn.Sequential(layers)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output = self.conv5(output)
+        output = self.avg_pool(output)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+
+        return output
+
+
+def resnet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def resnet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def resnet50():
+    return ResNet(BottleNeck, [3, 4, 6, 3])
+
+
+def resnet101():
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+
+def resnet152():
+    return ResNet(BottleNeck, [3, 8, 36, 3])
